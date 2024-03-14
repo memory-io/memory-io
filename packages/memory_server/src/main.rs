13a2d1cc37ddde7485444fb7e6e8e054fa59ae39
conner-replogle <95052372+conner-replogle@users.noreply.github.com>
2024-03-14 10:10:45 -0500

    duh

--- packages/memory_server/src/main.rs
+++ packages/memory_server/src/main.rs
@@ -5,20 +5,27 @@ use memory_server::{
     startup::{initialize_db, run, ServerConfig},
 };
 use mongodb::{options::ClientOptions, Client};
+use tracing::debug;
 
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
 
-    let client_options = ClientOptions::parse("mongodb://mongo_db:27017")
-        .await
-        .unwrap();
+    let client_options = ClientOptions::parse(
+        &std::env::var("MONGO_URI").unwrap_or("mongodb://localhost:27017".to_string()),
+    )
+    .await
+    .unwrap();
+
+    debug!("Connecting to database {client_options:?}");
     let address = TcpListener::bind("127.0.0.1:8000").unwrap();
 
     let db = Client::with_options(client_options).unwrap();
-    let mongo_database = MongoDatabase::new(db, "dev");
+    let mongo_database = MongoDatabase::new(
+        db,
+        &std::env::var("DATABASE_NAME").unwrap_or("dev".to_string()),
+    );
     initialize_db(&mongo_database).await.unwrap();
-
 
     run(mongo_database, address, ServerConfig::default())
         .await
