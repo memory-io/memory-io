1922dd23dc222a27e5d9573c7f8ef0af2641df32
conner-replogle <95052372+conner-replogle@users.noreply.github.com>
2024-05-14 16:11:03 -0500

    adding domain name

--- packages/memory_server/src/models/user/mod.rs
+++ packages/memory_server/src/models/user/mod.rs
@@ -176,8 +176,8 @@ pub(crate) async fn password_reset(
             .to(vec![("Password Reset", user.email.as_str())])
             .subject("Password Reset")
             .html_body(format!(
-                "Click <a href='http://{}/auth/password_reset/{}'>here</a> to reset your password",
-                option_env!("DOMAIN").unwrap_or("localhost:5173"),
+                "Click <a href='{}/auth/password_reset/{}'>here</a> to reset your password",
+                option_env!("DOMAIN").unwrap_or("http://localhost:5173"),
                 token
             ))
             .text_body("Hello world!");
